422bf7c3913f271b582282bd2247814648f12216
vinayakakbari02-lab <vinayakakbari02@gmail.com>
2026-01-07 08:58:35 +0530

    Create Vinayak.py

---
+++ Vinayak.py
@@ -0,0 +1,30 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a="hello world"
+>>> print(a)
+hello world
+>>> b=20
+>>> print(b)
+20
+>>> c=20.5
+>>> print(c)
+20.5
+>>> d=1j
+>>> print(d)
+1j
+>>> e=["apple","banana","cherry"]
+>>> print(e)
+['apple', 'banana', 'cherry']
+>>> f=("apple","banana","cherry")
+>>> print(f)
+('apple', 'banana', 'cherry')
+>>> g={"name" : "vinayak","age" : "17"}
+>>> print(g)
+{'name': 'vinayak', 'age': '17'}
+>>> h=True
+>>> i=False
+>>> print(h)
+True
+>>> print(i)
+False
+>>> 
