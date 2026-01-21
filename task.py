964ebe9de0743daf0357d05227a43cbc84d1595e
vinayakakbari02-lab <vinayakakbari02@gmail.com>
2026-01-21 08:41:58 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,55 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> p=int(input("Enter principle: "))
+Enter principle: 150
+>>> r=int(input("Enter rate: "))
+Enter rate: 2
+>>> t=int(input("Enter time: "))
+Enter time: 2
+>>> si=(p*r*t)/100
+>>> print("Simple Interest =",si)
+Simple Interest = 6.0
+>>> a=int(input("Enter first number: "))
+Enter first number: 5
+>>> b=int(input("Enter second number: "))
+Enter second number: 6
+>>> print("Maximum number is:",max(a,b))
+Maximum number is: 6
+>>> print(1\n2\n3\n4\n5)
+SyntaxError: unexpected character after line continuation character
+>>> print(1,2,3,4,5)
+1 2 3 4 5
+>>> s=input("Enter a string: ")
+Enter a string: 20
+>>> print("Length of string =",len(s))
+Length of string = 2
+>>> print("Welcome To Python")
+Welcome To Python
+>>> s=input("Enter a string: ")
+Enter a string: 25
+>>> print("First character:",s[0])
+First character: 2
+>>> s=input("Enter a string: ")
+Enter a string: 32
+>>> print("Last character:",s[-1])
+Last character: 2
+>>> num=int(input("Enter a number: "))
+Enter a number: 20
+>>> if num>0:
+	print("Positive number")
+	else:
+		
+SyntaxError: invalid syntax
+>>> a=int(input("Enter first number: "))
+Enter first number: 5
+>>> b=int(input("Eneter second number: "))
+Eneter second number: 6
+>>> c=int(input("Enter third number: "))
+Enter third number: 4
+>>> print("Sum =",a+b+c)
+Sum = 15
+>>> n=int(input("Enter a number: "))
+Enter a number: 45
+>>> print("Square =",n*n)
+Square = 2025
+>>> 
