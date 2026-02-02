e5aaefe0198e62520a138ea72a55fde3f65065bc
vinayakakbari02-lab <vinayakakbari02@gmail.com>
2026-02-02 10:20:28 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,36 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #int
+>>> print("15")
+15
+>>> print("54")
+54
+>>> #float
+>>> print("20.5")
+20.5
+>>> print("21.6")
+21.6
+>>> #bool
+>>> 0=True
+SyntaxError: can't assign to literal
+>>> 0=False
+SyntaxError: can't assign to literal
+>>> True=0
+SyntaxError: can't assign to keyword
+>>> A=True
+>>> B=False
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined
+>>> print(A)
+True
+>>> print(B)
+False
+>>> #str
+>>> print("Hello World")
+Hello World
+>>> print("Hello Vinayak")
+Hello Vinayak
+>>> 
