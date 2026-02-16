177d83d49393ba3eeaffe1e9eefe85c7d55f938b
vinayakakbari02-lab <vinayakakbari02@gmail.com>
2026-02-16 10:33:11 +0530

    Add files via upload

---
+++ Forloop.py
@@ -0,0 +1,42 @@
+"""#1.print number from 1 to 10
+for i in range (1,11) :
+    print(i)"""
+    
+"""#2.print even number from 1 to 20
+for i in range (1,21) :
+    if i % 2 == 0:
+        print(i)"""
+        
+"""#3.print odd number from 1 to 15
+for i in range (1,16) :
+    if i % 2!= 0 :
+        print(i)"""
+        
+"""#4.print table of 5
+for i in range (1,11) :
+        print("5x",i,"=",5*i)"""
+        
+"""#5.print characters of a string
+name="vinayak"
+for letter in name:
+    print (letter)"""
+    
+"""#6.sum of numbers from 1 to 15
+total=0
+for i in range (1,6):
+    total=total+i
+    print("sum is :",total)"""
+    
+"""#7.print list element
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)"""
+    
+"""#8.print numbers from 1 to 5
+for i in range (1,6):
+    print (i)"""
+    
+#9.print a massage 3 times
+for i in range(3):
+    print("Hello") 
+        
