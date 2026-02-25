a4d02e45b4d192180799b1c12801b2f16e1291ba
vinayakakbari02-lab <vinayakakbari02@gmail.com>
2026-02-25 08:22:35 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,46 @@
+"""#1.print number from 1 to 5
+i=1
+while i<=5 :
+    print(i)
+    i=i+1"""
+    
+"""#2.sum of numbers take user input
+n=int(input("Enter n:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum=",s)"""
+
+"""#3.print odd number between 1 and 20
+i=1
+while i <= 20:
+    if i % 2 != 0:
+        print(i)
+    i += 1"""
+    
+"""#4.print tabel of 4
+num=int(input("Enter Number:"))
+i=1
+while i <= 10:
+    print(num,"x",i,"=",num*i)
+    i=i+1"""
+    
+"""#5.print revese nuber
+i=2
+while i >=1:
+    print(i)
+    i=i-1"""
+    
+"""#6.find larjest number in list
+num=[10,20,30,40,50]
+largest=max(num)
+print(largest)"""
+
+"""#7.print even number between 1 to 20
+i=1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+    i=i+1"""
